2beb912ce88da0e8ead9b2ae060fe11fb967e06d
Mathieu <duc_mathieu@yahoo.fr>
2025-09-03 21:04:47 +0200

    clean test

--- test_script.py
+++ test_script.py
@@ -1,9 +1,11 @@
 import unittest
 from script import generer_affichage_texte
 
+
 class TestAffichageTexte(unittest.TestCase):
     def test_generer_affichage_texte(self):
         self.assertEqual(generer_affichage_texte(), "Bonjour, ceci est un texte affiché en sortie.")
+
 
 if __name__ == "__main__":
     unittest.main()

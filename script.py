7148f19efd168e6dc3dca8cf0ce7ac8aa51b85e2
Mathieu <duc_mathieu@yahoo.fr>
2025-09-03 20:59:43 +0200

    Add testing part

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
-print("Bonjour, ceci est un texte affiché en sortie.")
+def generer_affichage_texte():
+	return "Bonjour, ceci est un texte affiché en sortie."
+
+def main():
+	print(generer_affichage_texte())
+
+if __name__ == "__main__":
+	main()
---
+++ test_script.py
@@ -0,0 +1,9 @@
+import unittest
+from script import generer_affichage_texte
+
+class TestAffichageTexte(unittest.TestCase):
+    def test_generer_affichage_texte(self):
+        self.assertEqual(generer_affichage_texte(), "Bonjour, ceci est un texte affiché en sortie.")
+
+if __name__ == "__main__":
+    unittest.main()

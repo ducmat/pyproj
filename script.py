a100776f42da6296f7b65aef3271aac829b320fd
Mathieu <duc_mathieu@yahoo.fr>
2025-09-03 21:02:21 +0200

    cosmetic clean

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 def generer_affichage_texte():
-	return "Bonjour, ceci est un texte affiché en sortie."
+    return "Bonjour, ceci est un texte affiché en sortie."
 
 def main():
-	print(generer_affichage_texte())
+    print(generer_affichage_texte())
 
 if __name__ == "__main__":
-	main()
+    main()
